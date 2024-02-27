0ad812150b75fb59edd151704bddbf5ece9b455c
Dominic <dchang3419@hotmail.com>
2024-02-26 20:43:44 -0500

    Add fit results

--- src/imageDomain/best_NxCorr.jl
+++ src/imageDomain/best_NxCorr.jl
@@ -27,22 +27,38 @@ for data_file in readdir(inbase)
     end
 end
 
-model_info_list = ModelInfo.(Ref(eq_temp), Ref("Eq"), Ref(lower_eq), Ref(upper_eq), files, 0.0f0)
+#model_info_list = ModelInfo.(Ref(eq_temp), Ref("Eq"), Ref(lower_eq), Ref(upper_eq), files, 0.0f0)
+#imgfit.(model_info_list, maxevals)
+#
+#model_info_list = ModelInfo.(Ref(eq_blur_temp), Ref("EqBlur"), Ref(lower_eq), Ref(upper_eq), files, blur)
+#imgfit.(model_info_list, maxevals)
+#
+#model_info_list = ModelInfo.(Ref(dual_cone_temp), Ref("JBOX"), Ref(lower_dc), Ref(upper_dc), files, 0.0f0)
+#imgfit.(model_info_list, maxevals)
+#
+#model_info_list = ModelInfo.(Ref(dual_cone_blur_temp), Ref("JBOXBlur"), Ref(lower_dc), Ref(upper_dc), files, blur)
+#imgfit.(model_info_list, maxevals)
+#
+#model_info_list = ModelInfo.(Ref(eq_dual_cone_temp), Ref("EqDualCone"), Ref(lower_eq_dc), Ref(upper_eq_dc), files, 0.0f0)
+#imgfit.(model_info_list, maxevals)
+#
+#model_info_list = ModelInfo.(Ref(eq_dual_cone_blur_temp), Ref("EqDualConeBlur"), Ref(lower_eq_dc), Ref(upper_eq_dc), files, blur)
+#imgfit.(model_info_list, maxevals)
+
+files = String[]
+for data_file in readdir(inbase)
+    if (occursin("ma", data_file) || occursin("sa", data_file)) && occursin(".fits", data_file)
+        file_name = joinpath(inbase, data_file)
+        append!(files, [file_name])
+    end
+end
+
+#model_info_list = ModelInfo.(Ref(θ->eq_temp(θ, 1f0π)), Ref("Eq"), Ref(lower_eq), Ref(upper_eq), files, 0.0f0)[3]
+#imgfit.([model_info_list,], maxevals)
+
+model_info_list = ModelInfo.(Ref(θ-> dual_cone_temp(θ, 1f0π)), Ref("JBOX"), Ref(lower_dc), Ref(upper_dc), files, 0.0f0)
 imgfit.(model_info_list, maxevals)
 
-model_info_list = ModelInfo.(Ref(eq_blur_temp), Ref("EqBlur"), Ref(lower_eq), Ref(upper_eq), files, blur)
+model_info_list = ModelInfo.(Ref(θ-> eq_dual_cone_temp(θ, 1f0π)), Ref("EqDualCone"), Ref(lower_eq_dc), Ref(upper_eq_dc), files, 0.0f0)
 imgfit.(model_info_list, maxevals)
-
-model_info_list = ModelInfo.(Ref(dual_cone_temp), Ref("JBOX"), Ref(lower_dc), Ref(upper_dc), files, 0.0f0)
-imgfit.(model_info_list, maxevals)
-
-model_info_list = ModelInfo.(Ref(dual_cone_blur_temp), Ref("JBOXBlur"), Ref(lower_dc), Ref(upper_dc), files, blur)
-imgfit.(model_info_list, maxevals)
-
-model_info_list = ModelInfo.(Ref(eq_dual_cone_temp), Ref("EqDualCone"), Ref(lower_eq_dc), Ref(upper_eq_dc), files, 0.0f0)
-imgfit.(model_info_list, maxevals)
-
-model_info_list = ModelInfo.(Ref(eq_dual_cone_blur_temp), Ref("EqDualConeBlur"), Ref(lower_eq_dc), Ref(upper_eq_dc), files, blur)
-imgfit.(model_info_list, maxevals)
-
 
